328f0aca315dcf8c2748c60d02e14027b72d776d
yzx2019-ctrl <58676802+yzx2019-ctrl@users.noreply.github.com>
2021-02-23 18:15:04 +0800

    'first'

---
+++ scripts/test_case.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class TestCase(object):
+
+    def test_case_001(self):
+        assert 1
+
+    def test_case_002(self):
+        assert 1
+
+    def test_case_003(self):
+        assert 0
